1efc539f6c49d7892819372a1d6f9cee7d3bd6c1
Tim Sigafoos <tim@2gooddogs.com>
2025-04-17 20:45:33 -0700

    still testing

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World this is the test of render"}
+    return {"message": "Hello World this is the test of render."}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
